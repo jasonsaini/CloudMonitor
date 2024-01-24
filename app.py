5315362572895d3009952de1d6515bf0cf8ab211
Jason Saini <jasonsaini.exe@gmail.com>
2024-01-24 16:18:48 -0500

    Init app.py

---
+++ app.py
@@ -0,0 +1,23 @@
+import psutil
+from flask import Flask
+
+CPU_THRESHOLD = 80
+MEM_THRESHOLD = 80
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    cpu_percentage = psutil.cpu_percent()
+    mem_percentage = psutil.virtual_memory().percent
+    Message = None
+    if cpu_percentage > CPU_THRESHOLD:
+        Message = "High CPU Utilization"
+    if mem_percentage > MEM_THRESHOLD:
+        Message = "High Memory Utilization"
+    if mem_percentage > MEM_THRESHOLD and cpu_percentage > CPU_THRESHOLD:
+        message = "Hight Memory AND High CPU Utilization"
+    return f"CPU Utilization: {cpu_percentage}, Memory Utilization: {mem_percentage}"
+        
+if __name__ = '__main__':
+    app.run(debug = True, host= '0.0.0.0')
