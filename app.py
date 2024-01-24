4f32cba047ac734f620f357d749e92bdeb6c5f08
Jason Saini <jasonsaini.exe@gmail.com>
2024-01-24 16:26:37 -0500

    Fixed inaccurate CPU percent

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    cpu_percentage = psutil.cpu_percent()
+    cpu_percentage = psutil.cpu_percent(interval=1)
     mem_percentage = psutil.virtual_memory().percent
     Message = None
     if cpu_percentage > CPU_THRESHOLD:
